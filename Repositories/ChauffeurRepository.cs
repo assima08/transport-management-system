1a8cbba7b83a8c035bcfbbac458e0c84e447c34d
assima08 <eago92@gmail.com>
2026-05-25 17:35:15 -0400

    methodes finds pour le Repostory Chauffeur

--- Repositories/ChauffeurRepository.cs
+++ Repositories/ChauffeurRepository.cs
@@ -18,22 +18,40 @@ namespace TransportManagementSystem.Repositories
             _appDbContext = new AppDbContext();
         }
 
-        //TODO : AJOUT 
-
+        // Ajout par Objet
         public void  AjouterChauffeur(Chauffeur chauffeur)
         {
             _appDbContext.Chauffeurs.Add(chauffeur);
             _appDbContext.SaveChanges();
         }
-
+        //Ajout par attributs (fera appel a l'ajout par objet.
         public void AjouterEtcreerChauffeur(string p_nom, string p_Prenom, string p_Telephone, string p_NumeroPermis, DateOnly p_DateExpirationPermis)
         {
             Chauffeur chauffeur = new Chauffeur(p_nom,p_Prenom,p_Telephone,p_NumeroPermis,p_DateExpirationPermis);
-            _appDbContext.Chauffeurs.Add(chauffeur);
-            _appDbContext.SaveChanges();
+
+            AjouterChauffeur(chauffeur);
         }
 
-        //TODO : SUPPRESSION
+        //Methodes de recherches par attributs
+        public Chauffeur GetById(int p_Id)
+        {
+             Chauffeur it = _appDbContext.Chauffeurs.Find(p_Id);
+            return it;
+        }
+
+        public Chauffeur GetByTelephone(string p_telephone)
+        {
+            Chauffeur it = _appDbContext.Chauffeurs.FirstOrDefault(c=> c.Telephone == p_telephone);
+            return it;
+        }
+        public Chauffeur GetByNom(string p_nom, string p_prenom)
+        {
+            Chauffeur it = _appDbContext.Chauffeurs.FirstOrDefault(c => c.Nom == p_nom && c.Prenom == p_prenom);
+            return it;
+        }
+       
+
+        //suppression de l'objet.
         public void SupprimerChauffeur(Chauffeur chauffeur)
         {
             _appDbContext.Chauffeurs.Remove(chauffeur);
